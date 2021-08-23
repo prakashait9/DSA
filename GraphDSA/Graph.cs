ab6a69e4dad6eb88fe7820f45d6fe811f23f63f7
Prakash <prakashait@hotmail.com>
2021-08-23 17:08:28 +0530

    Create graph data structure

---
+++ GraphDSA/Graph.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+
+namespace GraphDSA
+{
+    public class Graph
+    {
+        private readonly LinkedList<int>[] _adj;
+
+        internal Graph(int length)
+        {
+            _adj = new LinkedList<int>[length];
+            for (var i = 0; i < length; i++)
+                _adj[i] = new LinkedList<int>();
+        }
+
+        public bool AddEdge(int source, int dest, bool undirected = false)
+        {
+            if (source >= _adj.Length || dest >= _adj.Length)
+                return false;
+
+            _adj[source].AddLast(dest);
+            if(undirected)
+                _adj[dest].AddLast(source);
+            return true;
+        }
+
+        public void Print()
+        {
+            for (var i = 0; i < _adj.Length; i++)
+            {
+                Console.Write($"[ {i} ] -> ");
+                foreach (var node in _adj[i])
+                {
+                    Console.Write($"[ {node} ] -> ");
+                }
+                Console.WriteLine($"#");
+            }
+        }
+    }
+}
